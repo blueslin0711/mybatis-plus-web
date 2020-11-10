c1b54cf8443d061bed6d2b1d54ee0c7a1e2364a1
blueslin0711 <coder_lin@foxmail.com>
2020-11-10 14:05:47 +0800

    新增actable 功能 根据实体类自动生成数据库表

--- src/main/java/com/blues/lin/mpauto/api/UpdateTableInfoController.java
+++ src/main/java/com/blues/lin/mpauto/api/UpdateTableInfoController.java
@@ -1,10 +1,8 @@
 package com.blues.lin.mpauto.api;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.stereotype.Controller;
-import com.blues.lin.mpauto.api.BaseController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * <p>
